5b6970ccfdaa2918ad01d7263fa0c69c47f3cdd1
Nikoleta Vlaskovic <Nikoleta Vlaskovic>
2017-09-22 15:13:55 +0200

    assignment1

---
+++ wp-content/themes/assignment1/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title><?php bloginfo('name'); ?>~<?php bloginfo('description'); ?></title>
+</head>
+<body>
+
+	<nav id="main-menu">
+		<ul>
+			<li><a href="#">Page</a></li>
+			<li><a href="#">Page</a></li>
+			<li><a href="#">Page</a></li>
+			<li><a href="#">Page</a></li>
+		</ul>
+	</nav>
+
